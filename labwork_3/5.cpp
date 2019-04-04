d760c1a0bda5b0540c0226a1001218d38d68ce44
andrey656 <andrey.plakin@bk.ru>
2019-04-04 18:31:42 +0300

    releas_3

--- labwork_3/5.cpp
+++ labwork_3/5.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <locale>
 using namespace std;
-wstring encrypt(const wstring text, const int key);
 wstring decrypt(const wstring text, const int key);
 int main()
 {
 	
 	locale loc("ru_RU.UTF-8");
 	locale::global(loc);
-	wstring wcod = L"арбуз"; 
+	wstring wcod = L"абабаба"; 
 	wstring wcod2 = L"", wdecod = L"", wdecod1 = L"";
-	int key, x=wcod2.size();
+	int key;
 	wcout << L"Введите ключ:";
 	cin >> key;
 	for (wchar_t & wc: wcod) {
@@ -19,39 +18,12 @@ int main()
 			wcod2 += wc;
 		}
 	}
-	for (int i = 0; i < x; i++)
-		if (wcod2[i] >= L'А' && wcod2[i] <= L'Я')
-			continue;
-		else {
-			wcout << L"ОШИБКА\n";
-			return 0;
-		}
-	if (wcod2 == L"") {
-		wcout << L"ОШИБКА\n";
-		return 0;
-	}
-	wcout << wcod2 << endl;
-	wdecod = encrypt(wcod2, key); 
-	wcout << wdecod << endl;
-	wdecod1 = decrypt(wdecod, key); 
+	
+	wdecod1 = decrypt(wcod, key); 
 	wcout << wdecod1 << endl;
 	return 0;
 }
-wstring encrypt(const wstring text, const int key)
-{
-	int key1;
-	wstring text1 = text;
-	wstring str;
-	if (key > 32) key1 = key % 32;
-	else key1 = key;
-	for (wchar_t & wc: text1) {
-		if ((wc + key1) > L'Я')
-			str += wc + key1 - 32;
-		else
-			str += wc + key1;
-	}
-	return str;
-}
+
 
 wstring decrypt(const wstring text, const int key)
 {
