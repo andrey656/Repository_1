ac6418b04de0482110aa8b86b4cb2c827660b2e1
andrey656 <andrey.plakin@bk.ru>
2019-04-10 21:24:11 +0300

    releas_4

--- labwork_3/1.cpp
+++ labwork_3/1.cpp
@@ -1,32 +1,44 @@
 #include <iostream>
 #include <string>
 #include <locale>
+#include <codecvt>
 using namespace std;
 int main()
 {
 	int num;
 	cout<<"Введите число от 1 до 100: ";
 	cin>>num;
-	locale loc("ru_RU.UTF-8");
-	string p = "В караване был" ;
-	string s = " верблюд";
+	string p = "В караване был X верблюд";
+	string k="был";
+	string l="верблюд";
+	string t = ("X");
 	if (num == 1 || (num > 20 && num % 10 == 1)) {
-		p.append(" ");
-		cout<<p;
-		cout<<num;
-		cout<<s<<endl;
-	} else if ( (num > 1 && num < 5) || ((num > 20) && (num % 10) > 1 && (num % 10) < 5)) {
-		p.append("о ");
-		s.append("а ");
-		cout<<p;
-		cout<< num;
-		cout<<s<<endl;
-	} else  {
-		p.append("о ");
-		s.append("ов");
-		cout<<p;
-		cout<< num;
-		cout<<s<<endl;
-
+		locale loc("ru_RU.UTF-8");
+		wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec;
+		p.replace(p.find(t), t.length(),to_string(num));
+		wstring wp = codec.from_bytes(p);
+		p = codec.to_bytes(wp);
+		cout<<p<<endl;
 	}
+	if ( (num > 1 && num < 5) || ((num > 20) && (num % 10) > 1 && (num % 10) < 5)) {
+		locale loc("ru_RU.UTF-8");
+		wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec;
+		p.replace(p.find(k), k.length(),"было");
+		p.replace(p.find(l), l.length(),"верблюда");
+		p.replace(p.find(t), t.length(),to_string(num));
+		wstring wp = codec.from_bytes(p);
+		p = codec.to_bytes(wp);
+		cout<<p<<endl;
+	}
+	 else if (num==0||(num>4&&num<21)||((num%10)>4)||(num%10==0)){
+		locale loc("ru_RU.UTF-8");
+		wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec;
+		p.replace(p.find(k), k.length(),"было");
+		p.replace(p.find(l), l.length(),"верблюдов");
+		p.replace(p.find(t), t.length(),to_string(num));
+		wstring wp = codec.from_bytes(p);
+		p = codec.to_bytes(wp);
+		cout<<p<<endl;
+	}
+	return 0;
 }
--- labwork_3/2.cpp
+++ labwork_3/2.cpp
@@ -3,23 +3,10 @@
 using namespace std;
 int main()
 {
-	int x;
-	string p = "deb http://mirror.mephi.ru/debian/ stretch main contrib non-free\ndeb-src http://mirror.mephi.ru/debian/ stretch main contrib non-free\ndeb http://security.debian.org/ stretch/updates main contrib non-free\ndeb-src http://security.debian.org/ stretch/updates main contrib non-free\ndeb http://mirror.mephi.ru/debian/ stretch-updates main contrib non-free\ndeb-src http://mirror.mephi.ru/debian/ stretch-updates main contrib non-free\ndeb http://mirror.mephi.ru/debian stretch-backports main contrib non-free\ndeb-src http://mirror.mephi.ru/debian stretch-backports main contrib non-free";
-	string v;
-	string t ="mephi";
-	string b ="yandex";
-	x = p.size();
-	for (int i = 0; i < x; i++) {
-		if (p.substr(i, t.size()) == t)
-			p.replace(i, t.size(), b);
-		if (p[i] == 'm' && p[i + 1] == 'e' && p[i + 2] == 'p' && p[i + 3] == 'h' && p[i + 4] == 'i') {
-			v += "yandex";
-			i += 4;
-			p.replace(i, i + 4, "yandex");
-		} else {
-			v += p[i];
-		}
+	string s="deb http://mirror.mephi.ru/debian/stretch main contrib non-free\ndeb-src http://mirror.mephi.ru/debian/ stretch main contrib non-free\ndeb http://security.debian.org/ stretch/updates main contrib non-free\ndeb-src http://security.debian.org/ stretch/updates main contrib non-free\ndeb http://mirror.mephi.ru/debian/ stretch-updates main contrib non-free\ndeb-src http://mirror.mephi.ru/debian/ stretch-updates main contrib non-free\ndeb http://mirror.mephi.ru/debian stretch-backports main contrib non-free\ndeb-src http://mirror.mephi.ru/debian stretch-backports main contrib non-free";
+	while (s.find("mephi")!=string::npos) {
+		s.replace(s.find("mephi"), 5, "yandex");
 	}
-	cout << p << endl;
+	cout<<s<<endl;
 	return 0;
 }
